0624e034c6111be3a4b000462d740377801a9734
pepper-grinder <94027236+pepper-grinder@users.noreply.github.com>
2023-04-03 18:51:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 function setup(){
-canvas = createCanvas(300,300);
-canvas.center();
-}
-
-function take_snapshot(){
-    name = document.getElementById("naming_input").value;
-    save(name+".jpg");
-}
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPose);
+    }
+    
+    function take_snapshot(){
+        image_name = document.getElementById("naming_input").value;
+        save(image_name+".jpg");
+    }
+    
+    function draw(){
+     image(video, 0,0, 300,300);
+    
+    }
+    
+    function modelLoaded(){
+    console.log("loaded");
+    }
+    
+    function gotPose(results){
+    if(results.length > 0){
+        console.table(results);
+        console.log("noseX =",results[0].pose.nose.x);
+        console.log("nosey =",results[0].pose.nose.y);
+    }
+    }
